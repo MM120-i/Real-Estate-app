9d027be95756ddcef8b770b2090163d2df9518fb
Mahim <mahimmarufuzzaman1234@gmail.com>
2025-04-28 19:16:32 -0400

    finsihed home screen UI

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,10 @@
+import { FeaturedCard, Card } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Text, View, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-// 1:50:01
 
 export default function Index() {
   return (
@@ -42,6 +42,30 @@ export default function Index() {
               </Text>
             </TouchableOpacity>
           </View>
+
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Filters />
+
+        {/* Recommendations */}
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
+          <Card />
         </View>
       </View>
     </SafeAreaView>
